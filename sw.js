303faa196e84bf707181561fdc6c285bed48a938
TejUzumaki <tejasgafat785@gmail.com>
2026-09-08 18:31:47 +0530

    patch: true 3rd-person cam based on bounds, wheel scaled to 25% bottom-right (McQueen)

--- sw.js
+++ sw.js
@@ -1,67 +1,24 @@
-const CACHE_NAME = 'aerof1-v4';
-
+const CACHE_NAME = 'f1-racer-v3';
 const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
-    './wheel.svg',
-
-    './assets/scene.gltf',
-    './assets/scene.bin',
-
-    './assets/textures/Meshpart1Mtl_baseColor.png',
-    './assets/textures/Meshpart2Mtl.004_baseColor.png',
-    './assets/textures/Meshpart4Mtl_baseColor.png',
-    './assets/textures/Meshpart5Mtl_baseColor.png',
-    './assets/textures/Meshpart6Mtl_baseColor.png',
-    './assets/textures/Meshpart7Mtl_baseColor.png',
-
-    './vendor/three.module.js',
-    './vendor/GLTFLoader.js',
-    './vendor/hands.js',
-    './vendor/drawing_utils.js',
-    './vendor/hand_landmark_full.tflite',
-    './vendor/hands_solution_packed_assets.data'
+  './',
+  './index.html',
+  './wheel.svg',
+  './manifest.json',
+  './assets/scene.gltf',
+  './assets/scene.bin',
+  './assets/textures/Meshpart1Mtl_baseColor.png',
+  './assets/textures/Meshpart2Mtl.004_baseColor.png',
+  './assets/textures/Meshpart4Mtl_baseColor.png',
+  './assets/textures/Meshpart5Mtl_baseColor.png',
+  './assets/textures/Meshpart6Mtl_baseColor.png',
+  './assets/textures/Meshpart7Mtl_baseColor.png'
 ];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS))
-            .then(() => self.skipWaiting())
-    );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        ).then(() => self.clients.claim())
-    );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))).then(() => self.clients.claim()));
 });
-
-self.addEventListener('fetch', event => {
-    if (event.request.method !== 'GET') return;
-
-    event.respondWith(
-        caches.match(event.request).then(cached => {
-            if (cached) return cached;
-
-            return fetch(event.request).then(response => {
-                if (response.ok) {
-                    const copy = response.clone();
-
-                    caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, copy);
-                    });
-                }
-
-                return response;
-            });
-        })
-    );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
 });
